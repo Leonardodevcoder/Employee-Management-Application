e3b9d8685af61ca2769747034c58e14ad3476f11
Leonardo Luan <leonardoluan2017@gmail.com>
2022-04-04 23:38:12 -0300

    first commit

---
+++ employee-management-system-backend/src/main/java/com/backend/employee/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.backend.employee.repository;
+
+import com.backend.employee.entity.EmployeeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
+}
---
+++ employee-management-system-backend/src/main/java/com/backend/employee/services/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.backend.employee.services;
+
+import com.backend.employee.model.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    Employee createEmployee(Employee employee);
+
+    List<Employee> getAllEmployees();
+
+    boolean deleteEmployee(Long id);
+
+    Employee getEmployeeById(Long id);
+
+    Employee updateEmployee(Long id, Employee employee);
+}
